9340dabffca5f9be52e1895bf95377218bde3cc3
Simón Fallas <sfv0215@gmail.com>
2021-11-03 18:29:14 -0600

    Final.

--- bnf.pl
+++ bnf.pl
@@ -1,5 +1,11 @@
 :- consult('database.pl').
 
+% REGLAS AUXILIARES
+
+% Se cumple si el elemento es miembro de la lista dada.
+miembro(Value, [Value|_]).
+miembro(Value, [_|Tail]) :- miembro(Value, Tail).
+
 % Reemplaza un elemento de una lista (Lista, Index, Elemento, NuevaLista).
 reemplazar_elemento([_|T], 0, X, [X|T]).
 reemplazar_elemento([H|T], I, X, [H|R]):-
@@ -9,11 +15,14 @@ reemplazar_elemento([H|T], I, X, [H|R]):-
 reemplazar_elemento(L, _, _, L).
 
 % Modicadores para cada requerimiento
-modificar_padecimiento(L, Elemento, NL):- reemplazar_elemento(L, 3, Elemento, NL).
+modificar_tipo(L, Elemento, NL):- reemplazar_elemento(L, 0, Elemento, NL).
+modificar_calorias(L, Elemento, NL):- reemplazar_elemento(L, 1, Elemento, NL).
+modificar_padecimiento(L, Elemento, NL):- reemplazar_elemento(L, 2, Elemento, NL).
+modificar_actividad(L, Elemento, NL):- reemplazar_elemento(L, 3, Elemento, NL).
 
 % Lee la informacion almacenada en la consulta para ser procesada y recomendar una dieta.
 leer_txt(L):-
-    open('list.txt',read,In),
+    open('requisitos.txt',read,In),
     repeat,
     read_line_to_codes(In,X),!,
     split_string(X, "\s", "\s", L),
@@ -21,18 +30,34 @@ leer_txt(L):-
 
 % Escribe un string en un archivo de texto.
 escribir_txt(Texto):-
-    open('list.txt', write, Stream),
+    open('requisitos.txt', write, Stream),
     write(Stream, Texto), nl,
     close(Stream).
 
 % Modifica los requisitos para seleccionar una dieta guardados en el archivo de texto
 modificar_requisitos(Elemento, Tipo):-
     leer_txt(L),
-    (Tipo = padecimiento -> 
+    ((Tipo = padecimiento -> 
         modificar_padecimiento(L, Elemento, NL),
         atomics_to_string(NL, " ", Texto),
+        escribir_txt(Texto));
+    (Tipo = calorias -> 
+        modificar_calorias(L, Elemento, NL),
+        atomics_to_string(NL, " ", Texto),
+        escribir_txt(Texto));
+    (Tipo = tipo -> 
+        modificar_tipo(L, Elemento, NL),
+        atomics_to_string(NL, " ", Texto),
+        escribir_txt(Texto));
+    (Tipo = actividad -> 
+        modificar_actividad(L, Elemento, NL),
+        atomics_to_string(NL, " ", Texto),
         escribir_txt(Texto))
-        .
+    ).
+
+
+
+
 
 % ----------------------- GRAMATICA -----------------------------------
 
@@ -41,34 +66,65 @@ determinante(["el"|S], S).
 determinante(["la"|S], S).
 determinante(["los"|S], S).
 determinante(["las"|S], S).
+determinante(["una"|S], S).
+determinante(["un"|S], S).
+determinante(["mi"|S], S).
+determinante(["de"|S], S).
+
 
 % NOMBRES
-nombre(["hombre"|S], S).
+
 nombre(["diabetes"|S], S):-modificar_requisitos("diabetes", padecimiento).
 nombre(["dislipidemia"|S], S):-modificar_requisitos("dislipidemia", padecimiento).
 nombre(["sobrepeso"|S], S):-modificar_requisitos("sobrepeso", padecimiento).
+nombre(["anemia"|S], S):-modificar_requisitos("anemia", padecimiento).
+nombre(["acidez"|S], S):-modificar_requisitos("acidez", padecimiento).
+
+nombre(["nivel"|S], S).
+nombre(["inicial"|S], S):-modificar_requisitos("inicial", actividad).
+nombre(["intermedio"|S], S):-modificar_requisitos("intermedio", actividad).
+nombre(["avanzado"|S], S):-modificar_requisitos("avanzado", actividad).
+
+nombre(["tipo"|S], S).
+nombre(["keto"|S], S):-modificar_requisitos("keto", tipo).
+nombre(["proteica"|S], S):-modificar_requisitos("proteica", tipo).
+nombre(["vegetariana"|S], S):-modificar_requisitos("vegetariana", tipo).
+nombre(["alcalina"|S], S):-modificar_requisitos("alcalina", tipo).
+nombre(["frugivora"|S], S):-modificar_requisitos("frugivora", tipo).
+
+nombre(["1500"|S], S):-modificar_requisitos("1500", calorias).
+nombre(["1800"|S], S):-modificar_requisitos("1800", calorias).
+nombre(["2000"|S], S):-modificar_requisitos("2000", calorias).
+nombre(["2500"|S], S):-modificar_requisitos("2500", calorias).
+nombre(["3000"|S], S):-modificar_requisitos("3000", calorias).
+
 nombre(["me"|S], S).
 nombre(["yo"|S], S).
+nombre(["nutritec"|S], S).
+nombre(["dieta"|S], S).
+nombre(["preferencia"|S], S).
 
 % VERBOS
-verbo(["correr"|S], S).
-verbo(["corro"|S], S).
-verbo(["caminar"|S], S).
-verbo(["camino"|S], S).
-verbo(["comer"|S], S).
-verbo(["como"|S], S).
-verbo(["corro"|S], S).
 verbo(["tengo"|S], S).
-verbo(["tener"|S], S).
-
-% ADJETIVOS
-
+verbo(["quiero"|S], S).
+verbo(["deseo"|S], S).
+verbo(["necesito"|S], S).
+verbo(["padezco"|S], S).
+verbo(["es"|S], S).
 
 
 % SALUDOS
-saludo(["hola"|S], S).
-saludo(["buenas"|S], S).
+saludar(["hola"|S], S).
+saludar(["buenas"|S], S).
+saludo(S0, S):-
+    saludar(S0, S1), sintagma_nominal(S1, S);
+    saludar(S0, S).
 
+% NEGACION
+negar(["no"|S], S).
+negacion(S0, S):-
+    negar(S0, S1), oracion(S1, S);
+    negar(S0, S).
 
 
 sintagma_nominal(S0, S):-
@@ -80,13 +136,83 @@ sintagma_verbal(S0, S):-
 
 oracion(S0, S):-
     sintagma_nominal(S0, S1), sintagma_verbal(S1, S);
-    saludo(S0, S1), sintagma_verbal(S1, S).
+    saludo(S0, S1), sintagma_verbal(S1, S);
+    sintagma_verbal(S0, S);
+    sintagma_nominal(S0, S);
+    negacion(S0, S).
 
 
 
-% Inicia el programa
-start :-
-    write("Bienvenido a NutriTEC: "),
+% --------------------- CONTROL DE FLUJO -------------------------
+
+
+% Solicita a la base de datos una dieta que cumpla con los requisitos del usuario
+extraer_lista_requisitos(R):-leer_txt(R).
+consultar_dieta:-
+        extraer_lista_requisitos([TipoString, CaloriasString, PadecimientoString, ActividadString]),
+
+        (TipoString = "tipo" -> Tipo = tipo; atom_string(Tipo, TipoString)),
+        (CaloriasString = "calorias" -> Calorias = calorias; atom_string(Calorias, CaloriasString)),
+        (PadecimientoString = "padecimiento" -> Padecimiento = padecimiento; atom_string(Padecimiento, PadecimientoString)),
+        (ActividadString = "actividad" -> Actividad = actividad; atom_string(Actividad, ActividadString)),
+        recomendar(_, [Tipo, Calorias, Padecimiento, Actividad], _).
+
+
+solicitar_actividad:-
+    write("\nDesea considerar su nivel de actividad fisica semanal?\n"),
+    write("[ inicial: 2 o menos dias, intermedio: 3 o 4 dias, avanzado: 5 o mas dias ]\n"),
     read(X),
     split_string(X, "\s", "\s", L),
-    oracion(L, []).
+    (miembro("inicial", L); miembro("intermedio", L); miembro("avanzado", L); miembro("no", L)),
+    oracion(L, []);
+    write("No se pudo encontrar sentido a la respuesta. Replanteando...\n\n"),
+    solicitar_actividad.
+
+solicitar_padecimiento:-
+    write("\nTiene algun padecimiento que se deba considerar?\n"),
+    write("(Puedo recomendar una dieta para: diabetes, dislipidemia, sobrepeso, anemia o acidez)\n"),
+    read(X),
+    split_string(X, "\s", "\s", L),
+    (miembro("diabetes", L); miembro("dislipidemia", L); miembro("sobrepeso", L); miembro("no", L);
+    miembro("anemia", L); miembro("acidez", L)),
+    oracion(L, []);
+    write("No se pudo encontrar sentido a la respuesta. Replanteando...\n\n"),
+    solicitar_padecimiento.
+
+solicitar_calorias:-
+    write("\nDesea que contenga una cantidad especifica de kilocalorias?\n"),
+    write("(Se admiten los siguientes valores: 1500, 1800, 2000, 2500, 3000)\n"),
+    read(X),
+    split_string(X, "\s", "\s", L),
+    (miembro("1500", L); miembro("1800", L); miembro("2000", L); miembro("no", L);
+    miembro("2500", L); miembro("3000", L)),
+    oracion(L, []);
+    write("No se pudo encontrar sentido a la respuesta. Replanteando...\n\n"),
+    solicitar_calorias.
+
+solicitar_tipo:-
+    write("\nExiste un tipo de dieta que sea de su preferencia?\n"),
+    write("(Puedo ofrecerle los siguientes tipos: keto, proteica, vegetariana, alcalina y frugivora.\n"),
+    read(X),
+    split_string(X, "\s", "\s", L),
+    (miembro("keto", L); miembro("proteica", L); miembro("vegetariana", L);
+    miembro("no", L); miembro("alcalina", L); miembro("frugivora", L)),
+    oracion(L, []);
+    write("No se pudo encontrar sentido a la respuesta. Replanteando...\n\n"),
+    solicitar_tipo.
+
+% Inicia el programa
+iniciar:-
+    escribir_txt("tipo calorias padecimiento actividad"), % Limpia la lista de requisitos
+    write("\nBienvenido a NutriTEC, en que le puedo ayudar?\n"),
+    read(X),
+    split_string(X, "\s", "\s", L),
+    miembro("dieta", L),
+    oracion(L, []),
+    write("Excelente, te hare unas preguntas para encontrar una dieta adecuada...\n\n"),
+    solicitar_tipo,
+    solicitar_calorias,
+    solicitar_padecimiento,
+    solicitar_actividad,
+    consultar_dieta;
+    write("No se pudo encontrar sentido a la oracion, o no se relaciona con dietas.\n").
--- database.pl
+++ database.pl
@@ -2,7 +2,7 @@
 % Tipos de dieta
 tipoDieta(keto).
 tipoDieta(proteica).
-tipoDieta(vegana).
+tipoDieta(vegetariana).
 tipoDieta(alcalina).
 tipoDieta(frugivora).
 
@@ -10,82 +10,104 @@ tipoDieta(frugivora).
 padecimiento([dislipidemia, "problemas del control del colesterol"]).
 padecimiento([diabetes, "problemas de insulina en la sangre"]).
 padecimiento([sobrepeso, "peso relativamente excesivo por acumulacion de grasa"]).
+padecimiento([anemia, "disminucion de globulos rojos"]).
+padecimiento([acidez, "niveles bajos de pH en el cuerpo"]).
 
 
-% Actividad fisica semanal: [nivel, dias minimos, dias maximos]
-nivelesActividad([inicial, 0, 2]).
-nivelesActividad([intermedio, 3, 4]).
-nivelesActividad([avanzado, 5, 7]).
+% Actividad fisica semanal
+nivelesActividad(inicial). % 2 o menos dias
+nivelesActividad(intermedio). % 3, 4 dias
+nivelesActividad(avanzado). % 5 o mas dias
 
 % Calorias
-calorias(1500).
-calorias(2000).
-calorias(2500).
-calorias(3000).
+calorias('1500').
+calorias('1800').
+calorias('2000').
+calorias('2500').
+calorias('3000').
 
 
 % Dietas: [Nombre, tipo, calorias, recomendaciones, detalles].
-dieta("Baja en grasa 1", tipoDieta(vegana), calorias(1500),
-                            padecimiento(sobrepeso), padecimiento(blanqueamiento), % Recomendado, No recomendado
-                            nivelesActividad(inicial), nivelesActividad(avanzado), % Recomendado, No recomendado
-                        "Desayuno1, Almuerzo1, Cena1").
+dieta("Dieta keto (2000 kcal)", tipoDieta(keto), calorias('2000'),
+        padecimiento(diabetes), padecimiento(acidez), % Recomendado, No recomendado
+        nivelesActividad(intermedio), nivelesActividad(inicial), % Recomendado, No recomendado
+        "Desayuno: 4 huevos revueltos con verduras\nRefrigerio: punado de nueces\nAlmuerzo: 1 taza albondigas a la italiana\nMerienda: Yogurt natural con almendras.\nCena: Hamburguesa de atun").
 
-dieta("Baja en grasa 2", tipoDieta(vegana), calorias(2000), 
-                            padecimiento(sobrepeso), padecimiento(blanqueamiento), % Recomendado, No recomendado
-                            nivelesActividad(inicial), nivelesActividad(avanzado), % Recomendado , No recomendado
-                    "Desayuno2, Almuerzo2, Cena2").
+dieta("Dieta vegetariana (1800 kcal)", tipoDieta(vegetariana), calorias('1800'), 
+        padecimiento(sobrepeso), padecimiento(anemia), % Recomendado, No recomendado
+        nivelesActividad(inicial), nivelesActividad(avanzado), % Recomendado , No recomendado
+        "Desayuno: 1 taza de yogurt desnatado con avena\nRefrigerio: 1 kiwi\nAlmuerzo: 1 taza de garbanzos con espinacas\nMerienda: Ensalada de frutas\nCena: 1 taza ensalada con queso y linaza").
 
-dieta("Baja en grasa 3", tipoDieta(vegana), calorias(2000), 
-                padecimiento(sobrepeso), padecimiento(blanqueamiento), % Recomendado, No recomendado
-                nivelesActividad(inicial), nivelesActividad(avanzado), % Recomendado , No recomendado
-        "Desayuno2, Almuerzo2, Cena2").
+dieta("Dieta proteica (3000 kcal)", tipoDieta(proteica), calorias('3000'), 
+        padecimiento(anemia), padecimiento(sobrepeso), % Recomendado, No recomendado
+        nivelesActividad(avanzado), nivelesActividad(inicial), % Recomendado , No recomendado
+        "Desayuno: 2 huevos, 1 taza de avena, cafe con leche\nRefrigerio: sandwich de jamon y verduras\nAlmuerzo: 200g pollo, 250g patatas y 1 aguacate\nMerienda: 1 manzana y sandiwch de queso\nCena: 200 g de salmon, 250g de arroz y verduras al gusto").
 
-dieta("Baja en grasa 4", tipoDieta(vegana), calorias(2000), 
-                padecimiento(sobrepeso), padecimiento(blanqueamiento), % Recomendado, No recomendado
-                nivelesActividad(inicial), nivelesActividad(avanzado), % Recomendado , No recomendado
-            "Desayuno2, Almuerzo2, Cena2").
+dieta("Dieta alcalina (2500 kcal)", tipoDieta(alcalina), calorias('2500'), 
+        padecimiento(acidez), padecimiento(dislipidemia), % Recomendado, No recomendado
+        nivelesActividad(intermedio), nivelesActividad(inicial), % Recomendado , No recomendado
+        "Desayuno: 1 tostada de pan integral con pavo, agua con limon\nRefrigerio: 1 manzana\nAlmuerzo: 1 plato de pasta integral con pescado, sumo de tomate\nMerienda: Batido de leche de almendras con fresas\nCena: Sopa con caldo de pollo, arroz integral y cebolla").
+
+dieta("Dieta frugivora (1500 kcal)", tipoDieta(alcalina), calorias('1500'), 
+        padecimiento(dislipidemia), padecimiento(anemia), % Recomendado, No recomendado
+        nivelesActividad(inicial), nivelesActividad(avanzado), % Recomendado , No recomendado
+        "Desayuno: Leche de coco, 1 platano, 1 taza de fresas\nRefrigerio: 1 manzana\nAlmuerzo: Ensalada de espinacas, pera y nueces, jugo de naranja\nMerienda: Punado de semillas de girasol y calabaza\nCena: Sopa de melon, limon y menta, higos").
+
+
+
+
+% REGLAS PARA SELECCIONAR DIETAS
 
 
 % Confirma que el tipo de dieta sea el mismo
 mismo_tipo(Tipo, Tipo2, RequisitosCumplidos):-
-    Tipo = Tipo2,
+    (Tipo = Tipo2; Tipo = tipoDieta(tipo)),
     RequisitosCumplidos is 1, !;
     RequisitosCumplidos is 0.
 
 % Confirma que las calorias sean las correctas para la dieta
 mismas_calorias(Cal1, Cal2, RequisitosCumplidos):-
-    Cal1 = Cal2,
+    (Cal1 = Cal2; Cal1 = calorias(calorias)),
     RequisitosCumplidos is 1, !;
     RequisitosCumplidos is 0.
 
 % Confirma que los padecimientos sean los correctos para la dieta
 padecimientos_adecuados(Pad, PadRec2, PadNoRec2, RequisitosCumplidos):-
-    (Pad = PadRec2; Pad \= PadNoRec2) -> 
+    (Pad = PadRec2; Pad \= PadNoRec2; Pad = padecimiento(padecimiento)) -> 
     RequisitosCumplidos is 1;
     RequisitosCumplidos is 0.
 
 % Confirma que los niveles de actividad sean los correctos para la dieta
-actividad_correcta(ActDeseada, ActNoDeseada, ActivRec2, ActNoRec2, RequisitosCumplidos):-
-    (ActDeseada = ActivRec2, ActNoDeseada \= ActivRec2) -> 
-    RequisitosCumplidos is 1;
-    (ActDeseada \= ActNoRec2, ActDeseada = ActivRec2) ->
+actividad_correcta(ActDeseada, ActivRec2, ActNoRec2, RequisitosCumplidos):-
+    (ActDeseada = ActivRec2; ActDeseada \= ActNoRec2; ActDeseada = nivelesActividad(actividad)) -> 
     RequisitosCumplidos is 1;
     RequisitosCumplidos is 0.
 
+
 % Recomienda una dieta con al menos 3 requisitos cumplidos
-recomendar([Nombre, Tipo, Cal, Pad, ActDeseada, ActNoDeseada, Detalles]) :-
+recomendar(Nombre, [Tipo, Cal, Pad, ActDeseada], Detalles) :-
     dieta(Nombre, Tipo2, Cal2, PadRec2, PadNoRec2, ActivRec2, ActNoRec2, Detalles),
 
-    mismo_tipo(Tipo, Tipo2, ReqCumplidos1),
-    mismas_calorias(Cal, Cal2, ReqCumplidos2),
-    padecimientos_adecuados(Pad, PadRec2, PadNoRec2, ReqCumplidos3),
-    actividad_correcta(ActDeseada, ActNoDeseada, ActivRec2, ActNoRec2, ReqCumplidos4),
+    mismo_tipo(tipoDieta(Tipo), Tipo2, ReqCumplidos1),
+    mismas_calorias(calorias(Cal), Cal2, ReqCumplidos2),
+    padecimientos_adecuados(padecimiento(Pad), PadRec2, PadNoRec2, ReqCumplidos3),
+    actividad_correcta(nivelesActividad(ActDeseada), ActivRec2, ActNoRec2, ReqCumplidos4),
 
     RequisitosCumplidos is ReqCumplidos1 + ReqCumplidos2 + ReqCumplidos3 + ReqCumplidos4,
+    RequisitosCumplidos = 4,
+    format("Dieta encontrada con 4 requisitos cumplidos:\n ~s\n~s \n", [Nombre, Detalles]), !.
 
-    ((RequisitosCumplidos = 4) ->
-    format("Dieta recomendada: ~s: ~s \nRequisitos cumplidos: ~w", [Nombre, Detalles, RequisitosCumplidos]), !;
-    (RequisitosCumplidos = 3) ->
-    format("Dieta recomendada: ~s: ~s \nRequisitos cumplidos: ~w", [Nombre, Detalles, RequisitosCumplidos])).
+recomendar(Nombre, [Tipo, Cal, Pad, ActDeseada], Detalles) :-
+    dieta(Nombre, Tipo2, Cal2, PadRec2, PadNoRec2, ActivRec2, ActNoRec2, Detalles),
+
+    mismo_tipo(tipoDieta(Tipo), Tipo2, ReqCumplidos1),
+    mismas_calorias(calorias(Cal), Cal2, ReqCumplidos2),
+    padecimientos_adecuados(padecimiento(Pad), PadRec2, PadNoRec2, ReqCumplidos3),
+    actividad_correcta(nivelesActividad(ActDeseada), ActivRec2, ActNoRec2, ReqCumplidos4),
+
+    RequisitosCumplidos is ReqCumplidos1 + ReqCumplidos2 + ReqCumplidos3 + ReqCumplidos4,
+    RequisitosCumplidos = 3,
+    format("Dieta encontrada con 3 requisitos cumplidos:\n ~s\n~s \n", [Nombre, Detalles]), !;
+    format("\nNo se encontro ninguna dieta que cumpla con al menos 3 de los requisitos solicitados.\n").
 
 
